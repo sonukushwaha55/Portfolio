9d67c7cc97f40abe5305ac68eaa87fede2fd496e
sonukushwaha55 <sonukumar1772001@gmail.com>
2025-02-07 22:59:31 +0530

    Add Footer component and update Contact styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,19 +13,19 @@ const Contact = () => {
         <div className="contact-section">
             <div className="contact-left">
                 <h1>Let's talk</h1>
-                <p>I'm currently avaliable to taken on new projects.so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+                <p>I'm currently avaliable to take on new projects.so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
                 <div className="contact-details">
                     <div className="contact-detail">
                     <i class="ri-mail-fill"></i>
-                    <h1>sonukumar1772001@gmail.com</h1>
+                    <p>sonukumar1772001@gmail.com</p>
                     </div>
                     <div className="contact-detail">
                     <i class="ri-phone-fill"></i>
-                    <h1>(+91) 7352476780</h1>
+                    <p>(+91) 7352476780</p>
                     </div>
                     <div className="contact-detail">
                     <i class="ri-map-pin-2-fill"></i>
-                    <h1>Bihar, India</h1>
+                    <p>Bihar, India</p>
                     </div>
                 </div>
             </div>
@@ -36,10 +36,9 @@ const Contact = () => {
                 <input type="text" placeholder='Enter your email' name="email" />
                 <label htmlFor="">Write your message here </label>
                 <textarea name="message" placeholder='Enter your message' rows="8" ></textarea>
-                <button type='submit' className="contact-submit">Submit</button>
-            </form>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form> 
         </div>
-      
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-line'>
+        <hr />   
+      <div className='footer'>
+        <p>© 2024 Sonu Kumar. All rights reserved.</p>
+       <div className="footer-right">
+        <p>Term of Services</p>
+        <p>Privacy Policy</p>
+        <p>Connect with me</p>
+       </div>
+    </div>
+    </div>
+  )
+}
+
+export default Footer

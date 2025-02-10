4106316bd85b1b5b6a73b3604ca796570a777424
sonukushwaha55 <sonukumar1772001@gmail.com>
2025-02-10 21:53:34 +0530

    Add responsive styles for About, Contact, Footer, Hero, MyWork, Navbar, and Services components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./Navbar.css"
 import underline from '../../assets/underline_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import 'remixicon/fonts/remixicon.css'
 
 const Navbar = () => {
 
 const [menu, setMenu] = useState("home")
+const menuRef = useRef();
+
+const openMenu = ()=>{
+  menuRef.current.style.right="0"
+}
+const closeMenu = ()=>{
+  menuRef.current.style.right="-350px"
+}
 
   return (
     <div className='navbar'>
         <img src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-letter-s-logo-png-png-image_5685888.png" alt="" />
-   <ul className="nav-menu">
+        <i onClick={openMenu} className="nav-open ri-menu-3-fill"></i>
+   <ul ref={menuRef} className="nav-menu">
+   <i onClick={closeMenu} className=" nav-close ri-close-large-fill"></i>
     <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline}alt=""/>:<></>}</li>
     <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline}alt=""/>:<></>}</li>
     <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={underline}alt=""/>:<></>}</li>
